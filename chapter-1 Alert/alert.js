f345649858760fc3476c5f08b867692bc87ce3e0
Nimraumer-dev <nimraumerfarooq@gmail.com>
2025-10-29 04:28:51 +0500

    first commit

---
+++ chapter-1 Alert/alert.js
@@ -0,0 +1,15 @@
+
+// Q1
+alert('Welcome to JavaScript Assignment Page');
+// Q2
+alert('Error! Please enter a valid password.');
+// Q3
+alert('Welcome to JS Land...\nHappy Coding!');
+// Q4
+alert('Welcome to JS Land');
+alert('Happy Coding');
+// Q5
+console.log("Hello... I can run JS through my web browser's console.");
+
+
+
---
+++ chapter-1 Alert/classpractice.js
@@ -0,0 +1,29 @@
+alert ("HELLO WORLD")
+alert ("SERVICES")
+
+
+
+alert("Hello")
+alert(4*4)
+alert("welcome to our website")
+alert("Your form has been submitted successfully!")
+alert(4+4-8*0)
+
+
+
+confirm('Are you under 18')
+confirm('Are you sure you want to delete this file?')
+confirm('Do you Really want to exit the page?')
+confirm('would you like to save changes before closing?')
+confirm('Do you agree with the terms and condition?')
+
+
+console.log(123456);
+console.log("The sum of number is", 10+20);
+console.log('User logged in Successfully');
+console.log("The subtraction of number is", 10-20);
+console.log(900-500);
+
+
+
+
