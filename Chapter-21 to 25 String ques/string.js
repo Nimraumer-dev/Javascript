73ae5d85cd8a76ded55f2e092550bb4a3e4794a7
Nimraumer-dev <nimraumerfarooq@gmail.com>
2025-12-06 05:40:00 +0500

    adding folder

---
+++ Chapter-21 to 25 String ques/string.js
@@ -0,0 +1,28 @@
+//1
+
+let firstName = prompt("First Name");
+
+let lastName = prompt("Last Name");
+
+let fullName = ( "Hello! " + firstName.toUpperCase() +" "+ lastName.toUpperCase() );
+
+console.log(fullName);
+
+
+
+//2
+
+let phone_model= prompt("Enter your favourite phone model");
+
+let model_length = phone_model;
+
+let model = (   "Length of String: " + model_length.length )  
+
+
+document.write("My favourite phone model is " + phone_model + "<br>")
+
+document.write(model);
+
+
+//3
+
